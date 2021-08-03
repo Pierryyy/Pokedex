7dea1115e2fa8f8b8c6605f8260837238972ad36
Pierryyy <83316323+Pierryyy@users.noreply.github.com>
2021-08-02 22:28:45 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/views/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'pokedex',
+    home: Home(),
+  ));
+}
---
+++ lib/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+Color redTheme = Color(0XFFE3350D);
+Color blueTheme = Color(0XFF31A7D7);
+Color orangeTheme = Color(0XFFF17F2E);
+Color rochaTheme = Color(0XFFA48C22);
